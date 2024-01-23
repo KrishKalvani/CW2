f89a5f92d62f1ebe91aa95405852c6f476ade3a2
Krish Kalvani <k4spidey@gmail.com>
2024-01-23 17:37:07 +0400

    Added package.json files. Installed express installed mongodb created server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+app.set('port', 3000)
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Origin-Headers", "Access-Control-Allow-Origin-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+    next();
+})
+
+const MongoClient = require('mongodb').MongoClient;
+
+
+// Connect to MongoDB
+let db;
+MongoClient.connect('mongodb+srv://krishKal:lionking@cluster0.hykpngx.mongodb.net', (err, client) => {
+    db = client.db('afterSchool');
+});
+
+// GET /lessons endpoint
+app.get('/lessons', (req, res) => {
+    db.collection('lessons').find({}).toArray((err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
